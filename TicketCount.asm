71a3a09ed7e7c50216c21a4f58731f8bce4c073f
Richard Tyshchenko <wromankosawp@gmail.com>
2017-03-14 12:13:23 +0200

    uses one procedure to check equality

--- TicketCount.asm
+++ TicketCount.asm
@@ -14,7 +14,7 @@ data32left DWORD ?
 data32right DWORD ?
 data32Count DWORD ?
 data32Iter DWORD ?
-data16Ten WORD ?
+data32Ten DWORD ?
 data32Div DWORD ?
 ifmt db "%d",0
 buf db BSIZE dup (?)
@@ -26,57 +26,17 @@ cWritten dd ?
 start:
     invoke GetStdHandle, STD_OUTPUT_HANDLE
     mov stdout, eax
-    mov data16Ten, 10
+    mov data32Ten, 10
     mov data32Count, 0
     mov edx, 1000
-    mov ecx, 999999
+    mov ecx, 1000000
     nxt:
         mov data32Iter, edx; int i, make a procedure then
 
-        mov data32left, 0
-        mov eax, edx
-        mov data32Div, 100000
-        mov edx, 0
-        div data32Div
-        add data32left, eax; result of i/100000
-        mov eax, data32Iter
-        mov data32Div, 10000
-        mov edx, 0
-        div data32Div
-        mov edx, 0
-        div data16Ten
-        add data32left, edx ; result of (i/10000)%10
-        mov eax, data32Iter
-        mov data32Div, 1000
-        mov edx, 0
-        div data32Div
-        mov edx, 0
-        div data16Ten
-        ;push edx ; result of (i/1000)%10
-        add data32left, edx
-        
-        mov data32right, 0
-        mov eax, data32Iter
-        mov edx, 0
-        div data16Ten
-        add data32right, edx; result of i%10
-        mov eax, data32Iter
-        mov edx, 0
-        div data16Ten
-        mov edx, 0
-        div data16Ten
-        add data32right, edx; result of (i/10)%10
-        mov eax, data32Iter
-        mov data32Div, 100
-        mov edx, 0
-        div data32Div
-        mov edx, 0
-        div data16Ten
-        add data32right, edx ; result of (i/100)%10
+        call CheckNums
 
         ;make %10 and other, eax /= 10, edx = eax%10
-        mov eax, data32right
-        cmp data32left, eax
+        cmp eax, 0
         jz CountInc
         jmp AllInc
         CountInc:
@@ -93,5 +53,51 @@ done:
     invoke WriteConsoleA, stdout, ADDR buf, BSIZE, ADDR cWritten, NULL
     invoke WriteConsoleA, stdout, ADDR crlf, 2, ADDR cWritten, NULL
 invoke ExitProcess, 0
-end start
 
+CheckNums proc
+    mov data32left, 0
+    mov eax, edx
+    mov data32Div, 100000
+    mov edx, 0
+    div data32Div
+    add data32left, eax; result of i/100000
+    mov eax, data32Iter
+    mov data32Div, 10000
+    mov edx, 0
+    div data32Div
+    mov edx, 0
+    div data32Ten
+    add data32left, edx ; result of (i/10000)%10
+    mov eax, data32Iter
+    mov data32Div, 1000
+    mov edx, 0
+    div data32Div
+    mov edx, 0
+    div data32Ten
+    ;push edx ; result of (i/1000)%10
+    add data32left, edx
+
+    mov data32right, 0
+    mov eax, data32Iter
+    mov edx, 0
+    div data32Ten
+    add data32right, edx; result of i%10
+    mov eax, data32Iter
+    mov edx, 0
+    div data32Ten
+    mov edx, 0
+    div data32Ten
+    add data32right, edx; result of (i/10)%10
+    mov eax, data32Iter
+    mov data32Div, 100
+    mov edx, 0
+    div data32Div
+    mov edx, 0
+    div data32Ten
+    add data32right, edx ; result of (i/100)%10
+
+    mov eax, data32right
+    sub eax, data32left
+    ret 
+CheckNums endp
+end start
